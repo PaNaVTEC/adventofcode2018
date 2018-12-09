f64bcf9e3d13bed377874e277622a21e9cd9df5b
Christian Panadero <panavtec@gmail.com>
2018-12-09 12:36:02 +0000

    day5 part2

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,29 +1,42 @@
 module Day5 where
 
-import Data.Char
-import Debug.Trace
+import Data.Char (toUpper, isUpper, isLower)
 import Data.Function (fix)
+import Data.Bool (bool)
+import Data.List (minimum)
 
 mainPart1 :: IO ()
 mainPart1 = do
   contents <- readFile "./input/day5.txt"
-  print $ length $ makePolymerReact contents
+  print $ length $ makePolymerReact (trim contents)
+
+trim :: String -> String
+trim = unwords . words
 
 makePolymerReact :: String -> String
 makePolymerReact = idempotently polymerIteration
   where
-  polymerIteration = fst . foldl foo ("", (' ', ""))
-  foo (acc, (lst, accNoAdd)) cchr =
-     if react lst cchr
-     then (accNoAdd, (' ', accNoAdd))
-     else (acc ++ [cchr], (cchr, acc))
+  polymerIteration = reverse . fst . foldl foo ("", (' ', ""))
+  foo (acc, (lst, accNoAdd)) cchr = bool
+    (cchr : acc, (cchr, acc))
+    (accNoAdd, (' ', accNoAdd))
+    (react lst cchr)
 
 react :: Char -> Char -> Bool
-react c c' =
-  (toUpper c == toUpper c')
+react c c' = (toUpper c == toUpper c')
   && ((isUpper c && isLower c') || (isLower c && isUpper c'))
 
 idempotently :: Eq a => (a -> a) -> a -> a
 idempotently = fix $ \i f a ->
   let a' = f a
   in if a' == a then a else i f a'
+
+mainPart2 :: IO ()
+mainPart2 = do
+  contents <- readFile "./input/day5.txt"
+  print $ shorterPolymer (trim contents)
+
+shorterPolymer :: String -> Int
+shorterPolymer contents = minimum $ polymerLengthWithout <$> ['A'..'Z']
+  where
+    polymerLengthWithout polymer = length . makePolymerReact $ filter ((/= polymer). toUpper) contents
